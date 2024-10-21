f2c68963b762bbe6e05bb09ca24edb02a0696344
dany <danychen.me@gmail.com>
2024-10-21 16:51:10 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const fs = require('fs');
+const morgan = require('morgan');
+const routes = require('./routes');
+const app = express();
+const PORT = 3000;
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./assets'));
+app.use(morgan('tiny'));
+// routes set up
+app.use(routes.indexRoutes);
+app.use('/users', routes.userRoutes);
+
+app.engine('dc', (filePath, options, callback) => {
+  fs.readFile(filePath, (err, tpl) => {
+    if (err) {
+      return callback(err);
+    } else {
+      const rendered = tpl
+        .toString()
+        .replaceAll('#title#', options.title)
+        .replace('#content#', options.content);
+
+      return callback(null, rendered);
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at port: ${PORT}.`);
+});
