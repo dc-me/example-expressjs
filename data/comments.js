fe926f80f74d635751669284e332eb455f084db1
dany <danychen.me@gmail.com>
2024-10-24 19:45:24 -0700

    mock data for comments

---
+++ data/comments.js
@@ -0,0 +1,22 @@
+const comments = [
+  {
+    id: 1,
+    userId: 2,
+    postId: 1,
+    body: 'Cool cool cool',
+  },
+  {
+    id: 2,
+    userId: 1,
+    postId: 3,
+    body: 'Way to go.',
+  },
+  {
+    id: 3,
+    userId: 1,
+    postId: 5,
+    body: 'This is good.',
+  },
+];
+
+module.exports = comments;
