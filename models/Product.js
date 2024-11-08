be0e03c1b8f2514b0d1ebb3379f26b0466698c72
dany <danychen.me@gmail.com>
2024-11-07 19:11:28 -0800

    product model

---
+++ models/Product.js
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose';
+
+export const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    picture: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model('Product', productSchema);
