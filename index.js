bdea185b9b5efc314144c4a0810124a7b090fa53
dany <danychen.me@gmail.com>
2024-11-21 19:42:05 -0800

    use cors and express session middleware

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import express from 'express';
 import fs from 'fs';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import passport from 'passport';
 import mongoose from 'mongoose';
+import cors from 'cors';
+import './utils/local-strategy.js';
 import configureRoutes from './routes/index.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,13 +20,31 @@ try {
 } catch (err) {
   console.error(err);
 }
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true, // ! important
+  // methods: ["GET", "POST"],
+};
+
 app.set('views', './views');
 app.set('view engine', 'ejs');
+app.use(express.static('./assets'));
+app.use(morgan('tiny'));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static('./assets'));
-app.use(morgan('tiny'));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    // cookie: { secure: true }, // works only on https
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 // routes set up
 configureRoutes(app);
 
